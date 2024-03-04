5e55d2c6d31d66fb5aa28f74992108e78a829316
Paul Morgan <orbitingmind@gmail.com>
2024-03-03 19:39:21 -0500

    added link from home page to profile page

--- hoopapp/src/componets/Top.js
+++ hoopapp/src/componets/Top.js
@@ -1,16 +1,19 @@
 import react from "react"   
 import "./Top.css" 
-import pIcon from "./images/ProjectH.svg"
+import pIcon from "./images/ProjectH.svg" 
+import { useParams ,useNavigate } from "react-router-dom"; 
 
 
  const  Top = () => {
+
+    const navigate = useNavigate();
 
         return(
                 <div className="top"> 
                     <img src={pIcon} className="icon"></img>  
 
 
-                    <div className="user-icon"></div>
+                    <div className="user-icon" onClick={((e)=> { navigate("/profile") })}></div>
 
                 </div>
         )
