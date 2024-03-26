cdd4d8deae3fbfe5b234f5edad265fc3b9742f2e
Paul Morgan <orbitingmind@gmail.com>
2024-03-25 22:30:55 -0400

    made some changes to the profile page

--- hoopapp/src/componets/pages/ProfilePage.js
+++ hoopapp/src/componets/pages/ProfilePage.js
@@ -24,28 +24,22 @@ const ProfilePage = ()=>{
                         <div className="pic-box"> H </div>
                     </div>  
 
-                    <div className="p-card-bottom">
+                    <div className="p-card-bottom"> 
 
-                        <div className="inner-card-b">
-                            <h1>My Favorite Park.</h1> 
-                            <p>demo park</p>
-                        </div>  
+                        <div className="test-box"> 
+                        
 
-                        <div className="inner-card-b"> 
-                        <h1>My City.</h1> 
-                        <p>Demo City DC</p>
-                            
-                        </div>  
-
-                        <div className="inner-card-b">
-                        <h1>Latest  Review</h1>
                         </div>
 
-
+                    
+                       
                         <button className="ep-btn">Edit Profile</button>
+
                     </div>
 
 
+
+                    
 
                 </div>
             
