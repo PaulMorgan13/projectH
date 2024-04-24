63ad3ec473e2fe6da53146cf162f4aea4ac5711a
Paul Morgan <orbitingmind@gmail.com>
2024-04-24 00:34:06 -0400

    added new sidecomponent

---
+++ hoopapp/src/componets/SideNav.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {Link } from "react-router-dom" 
+import homeIcon from "./images/home-icon.svg" 
+import addIcon from "./images/add-icon.svg"
+import searchIcon from "./images/search-icon.svg" 
+
+
+
+export default function SideNav () {
+
+        return (
+                    <div className="SideNav"> 
+
+
+                                
+            <ul class> 
+            <Link to={`/`} style={{ textDecoration: 'none'}}>
+              <li><img className='i' src={homeIcon}></img></li>  
+            </Link>
+            <Link to={`/add`} style={{textDecoration:"none"}}>
+              <li><img className='i'   src={addIcon}></img></li>  
+            </Link> 
+            <Link to={`/search`} style={{textDecoration:"none"}}>
+              <li><img className='i'  src={searchIcon}></img></li>  
+            </Link>
+
+             
+
+            </ul>
+                    
+                    
+                    
+                    
+                    
+                    </div>
+
+
+
+
+
+
+
+
+        )
+
+}
